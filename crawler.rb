8926db9872d8ed851702a01f1989cd73c391e4d9
Alexande B <abobrikovich@gmail.com>
2023-04-05 17:41:52 +0200

    Move to RelatonIana::DataFetcher.fetch instead system call

--- crawler.rb
+++ crawler.rb
@@ -1,11 +1,5 @@
 require 'fileutils'
-
-t1 = Time.now
-puts "Started at: #{t1}"
+require 'relaton_iana'
 
 FileUtils.rm_rf("data")
-system("relaton fetch-data iana-registries")
-
-t2 = Time.now
-puts "Stopped at: #{t2}"
-puts "Done in: #{(t2 - t1).round} sec."
+RelatonIana::DataFetcher.fetch
