d29e00601a3525678ed5639c8b8ec9ba3613caaa
Andrei Kislichenko <android.2net@gmail.com>
2023-05-24 19:14:20 -0400

    add index

--- crawler.rb
+++ crawler.rb
@@ -3,7 +3,12 @@ require 'relaton_iana'
 
 # token = ARGV.shift
 
+FileUtils.rm(Dir.glob("index*"))
+
 system("git clone https://github.com/ietf-tools/iana-registries.git iana-registries")
 
 FileUtils.rm_rf("data")
 RelatonIana::DataFetcher.fetch
+
+system "zip index-v1.zip index-v1.yaml"
+system "git add index-v1.zip index-v1.yaml"
