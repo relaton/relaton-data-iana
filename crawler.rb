bd31719b48ca20fdceca792dd33ed4859d774dd3
Andrei Kislichenko <android.2net@gmail.com>
2023-06-07 15:52:56 -0400

    Update crawler.rb

--- crawler.rb
+++ crawler.rb
@@ -9,6 +9,3 @@ system("git clone https://github.com/ietf-tools/iana-registries.git iana-registr
 
 FileUtils.rm_rf("data")
 RelatonIana::DataFetcher.fetch
-
-system "zip index-v1.zip index-v1.yaml"
-system "git add index-v1.zip index-v1.yaml"
